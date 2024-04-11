3eda2f9f94a086ec8e7ec262eaf977959611bccf
Zuza <windstrike@bk.ru>
2024-04-11 14:54:37 +0300

    lesson2task3,4

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] array = { 1, 20, 23, 85, 12, 1, 2, 6, 9, 8 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.WriteLine($"{array[i]} ");
+    }
+    i = i + 1;
+}
---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = 5;
+int[] array = { 2, 3, 8, 74, 5 };
+int i = 0;
+int max = array[0];
+while (i<n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+  if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
